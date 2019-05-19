ac4153239f57ae8cfffed41dd7e580242c3ab2de
xxssyyyyssxx <xxssyyyyssxx@126.com>
2019-05-19 21:52:51 +0800

    MultiValueMap新增forEachKeyValue功能

--- src/main/java/top/jfunc/common/utils/MultiValueMap.java
+++ src/main/java/top/jfunc/common/utils/MultiValueMap.java
@@ -1,12 +1,16 @@
 package top.jfunc.common.utils;
 
+import java.util.ConcurrentModificationException;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
+import java.util.function.BiConsumer;
 
 /**
  * Extension of the {@code Map} interface that stores multiple values.
- *
+ * 在原来spring的基础上新增了一些特性
  * @author Arjen Poutsma
+ * @author xiongshiyan
  * @since 3.0
  */
 public interface MultiValueMap<K, V> extends Map<K, List<V>> {
@@ -52,6 +56,27 @@ public interface MultiValueMap<K, V> extends Map<K, List<V>> {
 
     /**
      * 获取Map，value为list
+     * @return map
      */
     Map<K, List<V>> getMap();
+
+    /**
+     * 对每一个key、value遍历
+     * @param action 动作
+     */
+    default void forEachKeyValue(BiConsumer<? super K, ? super V> action) {
+        Objects.requireNonNull(action);
+        for (Map.Entry<K, List<V>> entry : entrySet()) {
+            K k;
+            List<V> vs;
+            try {
+                k = entry.getKey();
+                vs = entry.getValue();
+            } catch(IllegalStateException ise) {
+                // this usually means the entry is no longer in the map.
+                throw new ConcurrentModificationException(ise);
+            }
+            vs.forEach(v -> action.accept(k, v));
+        }
+    }
 }
