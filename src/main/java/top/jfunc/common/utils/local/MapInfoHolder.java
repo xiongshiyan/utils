1e879747db8a1ac1c3803ae7da5862d62cf207ea
xxssyyyyssxx <xxssyyyyssxx@126.com>
2020-04-26 16:20:52 +0800

    因为每个线程操作的Map是线程私有的，所以并没必要用锁

--- src/main/java/top/jfunc/common/utils/local/MapInfoHolder.java
+++ src/main/java/top/jfunc/common/utils/local/MapInfoHolder.java
@@ -8,7 +8,6 @@ import java.util.Map;
  * @author xiongshiyan at 2017/12/27
  */
 public class MapInfoHolder<K,V> extends InfoHolder<Map<K,V>> {
-
     public MapInfoHolder(){
         //既然是保存k-v类型的数据，所以事先就初始化，在使用的时候就没必要判空了，
         //因为每个线程都是对应独立的map，所以并没有必要使用线程安全的集合类，比如ConcurrentHashMap、HashTable，
@@ -21,7 +20,7 @@ public class MapInfoHolder<K,V> extends InfoHolder<Map<K,V>> {
      * @param key key
      * @param value value
      */
-    public synchronized void add(K key ,V value){
+    public void add(K key ,V value){
         Map<K, V> map = holder.get();
         map.put(key,value);
         //holder.set(map);
@@ -31,18 +30,18 @@ public class MapInfoHolder<K,V> extends InfoHolder<Map<K,V>> {
      * 根据key获取当前线程的值
      * @param key key
      */
-    public synchronized V get(K key){
+    public V get(K key){
         Map<K, V> map = holder.get();
         return map.get(key);
     }
 
-    public synchronized void remove(K key){
+    public void remove(K key){
         Map<K, V> map = holder.get();
         map.remove(key);
     }
 
     @Override
-    public synchronized void clear(){
+    public void clear(){
         Map<K, V> map = holder.get();
         map.clear();
 
