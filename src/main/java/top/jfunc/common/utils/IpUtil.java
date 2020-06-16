38f69bc2e386349ca28ea524d0d06dcf9dba262f
xxssyyyyssxx <xxssyyyyssxx@126.com>
2020-06-16 22:02:55 +0800

    getClientIp方法封装

--- src/main/java/top/jfunc/common/utils/IpUtil.java
+++ src/main/java/top/jfunc/common/utils/IpUtil.java
@@ -52,7 +52,7 @@ public class IpUtil {
      * 因为基于Servlet的和其他的框架例如vertx的获取方式不一样
      * @author xiongshiyan at 2020/6/16 , contact me with email yanshixiong@126.com or phone 15208384257
      */
-    public static interface IpInfoGetter {
+    public interface IpInfoGetter {
         /**
          * 从请求中获取header
          * @see HttpServletRequest
